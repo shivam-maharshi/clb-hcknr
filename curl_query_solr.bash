40219ea3910c7617c6edc0c2de4090d623a06f6c
Tingting Jiang <tvj@vt.edu>
2016-02-12 14:26:35 -0500

    Solr scipts on basic running commands and query parameters

---
+++ curl_query_solr.bash
@@ -0,0 +1,10 @@
+# search for all the documents for specific term: q=term
+curl http://localhost:8983/solr/gettingstarted/select?q=video
+# search for term in the specific field only: q=fieldname:term
+curl http://localhost:8983/solr/gettingstarted/select?q=name:black
+# show the results only contain specific fields: fl=fieldname
+curl http://localhost:8983/solr/gettingstarted/select?q=video&fl=id,name,price
+# search for fields in certain ranges: q=fieldname:[* To *]
+curl http://localhost:8983/solr/gettingstarted/select?q=price%3D0+TO+400&fl=id,name,price
+# Faceting search
+curl http://localhost:8983/solr/gettingstarted/select?q=price%3D0+TO+400&fl=id&facet=true&facet.field=cat
---
+++ run_solr.bash
@@ -0,0 +1,15 @@
+# start Solr and listenning on port 8983 (default)
+# can specifiy a server directory: bin/solr start -d newServerDir
+bin/solr start -p 8983
+# stop Solr
+bin/solr stop -p 8983
+# restart Solr
+bin/solr restart
+# create a Solr core before indexing and searching
+bin/solr -c gettingstarted
+# use Solr command line tool (post script) to index different types of documents
+bin/post -c gettingstared example/exampledocs/*.xml
+# check status
+bin/solr status
+# go to browser and see the Admin Console page
+open http://localhost:8983/solr/
