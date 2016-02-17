0ad5aa5a67905e209fc8a22f2a907a1ea6719e45
Tingting Jiang <tvj@vt.edu>
2016-02-16 21:45:54 -0500

    Add Solr core deletion

--- run_solr.bash
+++ run_solr.bash
@@ -6,9 +6,11 @@ bin/solr stop -all
 # restart Solr
 bin/solr restart
 # create a Solr core before indexing and searching
-bin/solr create -c collection1
+bin/solr create -c gettingstarted
+# delete a Solr core
+# bin/solr delete -c gettingstarted
 # use Solr command line tool (post script) to index different types of documents
-bin/post -c collection1 example/exampledocs/*.xml
+bin/post -c gettingstarted  example/exampledocs/*.xml
 # check status
 bin/solr status
 # go to browser and see the Admin Console page
