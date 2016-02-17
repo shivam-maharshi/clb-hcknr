f691503ac6630b64385285a12f30ba323a453270
Tingting Jiang <tvj@vt.edu>
2016-02-16 21:10:12 -0500

    fix core name typo

--- run_solr.bash
+++ run_solr.bash
@@ -2,13 +2,13 @@
 # can specifiy a server directory: bin/solr start -d newServerDir
 bin/solr start -p 8983
 # stop Solr
-bin/solr stop -p 8983
+bin/solr stop -all
 # restart Solr
 bin/solr restart
 # create a Solr core before indexing and searching
-bin/solr -c gettingstarted
+bin/solr create -c collection1
 # use Solr command line tool (post script) to index different types of documents
-bin/post -c gettingstared example/exampledocs/*.xml
+bin/post -c collection1 example/exampledocs/*.xml
 # check status
 bin/solr status
 # go to browser and see the Admin Console page
