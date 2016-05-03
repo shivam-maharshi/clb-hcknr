5b88c9f9095e43d09b44cf084fa971592fb0e645
Shivam Maharshi <shivam.maharshi@gmail.com>
2016-05-03 19:12:14 -0400

    Corrected version

--- install-rkt.sh
+++ install-rkt.sh
@@ -1,3 +1,4 @@
-wget https://github.com/coreos/rkt/releases/download/v1.5.0/rkt-v1.5.0.tar.gz
-tar xzvf rkt-v1.5.0.tar.gz
-cd rkt-v1.5.0
+wget https://github.com/coreos/rkt/releases/download/v1.3.0/rkt-v1.3.0.tar.gz
+tar xzvf rkt-v1.3.0.tar.gz
+mv rkt-v1.3.0  rkt
+cd rkt
