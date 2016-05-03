eacfab05564afe28084761cea37fb07452c18c2c
Shivam Maharshi <shivam.maharshi@gmail.com>
2016-05-03 01:48:13 -0400

    Added a shell script to automatically install Docker for Ubuntu Trusty

---
+++ install-docker.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo touch /etc/apt/sources.list.d/docker.list
+sudo echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get install docker-engine
+sudo service docker start
+sudo docker run hello-world
