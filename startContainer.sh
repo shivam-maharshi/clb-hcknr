ac81fe331e163a0d95dc00d638c82be0791c2d7e
Shivam Maharshi <shivam.maharshi@gmail.com>
2016-05-03 20:03:11 -0400

    Added dns configure.

--- startContainer.sh
+++ startContainer.sh
@@ -1,4 +1,4 @@
 # Docker
 sudo docker run -i --net=host -p 8080:8080 docker://shivammaharshi/mediawiki /bin/bash
 # rkt
-./rkt run --interactive=true --net=host --insecure-options=image docker://shivammaharshi/mediawiki --exec /bin/bash
+./rkt run --interactive=true --net=host --dns=8.8.8.8 --insecure-options=image docker://shivammaharshi/mediawiki --exec /bin/bash
