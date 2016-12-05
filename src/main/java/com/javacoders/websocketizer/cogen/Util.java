c691f8d585f818b3c8e69451e664fe25a0afb1bb
shivam-maharshi <shivam.maharshi@gmail.com>
2016-12-05 02:28:00 -0500

    Added functionality interface to pass arguments.

---
+++ src/main/java/com/javacoders/websocketizer/cogen/Util.java
@@ -0,0 +1,16 @@
+package com.javacoders.websocketizer.cogen;
+
+import java.util.Map;
+
+/**
+ * Hold the utils methods.
+ * 
+ * @author shivam.maharshi
+ */
+public class Util {
+  
+  public static String parseMessage(String message) {
+    return null;
+  }
+
+}
